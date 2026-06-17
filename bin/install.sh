f11c1028ab61ef9822c5bbcef88368e025bb87c8
AntELO1234 <for.servises.and.another@gmail.com>
2026-06-17 18:52:28 +0300

    Create install.sh

---
+++ bin/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run program under sudo"
+  exit 1
+fi
+
+install -m 755 font-mgr /usr/local/bin/font-mgr
+mkdir -p /etc/font-mgr
+echo "https://github.com/AntELO1234/font-mgr/fonts/%TYPE%/%LANG%/%NAME%.tar" > /etc/font-mgr/repos.cfg
+mkdir -p /tmp/font-mgr
+echo "Program installed"
